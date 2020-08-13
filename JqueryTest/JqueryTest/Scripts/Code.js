467f69e3cdbd9a6c91deecf780b9eec3b3a46b78
usuario <usuario@user.com>
2020-08-13 11:28:47 -0300

    agregado la carpeta del tp1

---
+++ JqueryTest/JqueryTest/Scripts/Code.js
@@ -0,0 +1,30 @@
+﻿var i = 0;
+var name = "diap_";
+
+$(document).ready(function () {
+    console.info("ready2");
+
+    $("#arrowLeft").click(function () {
+        if (i - 1 >= 0) {
+
+
+            $("#" + name + i).show("slide", { direction: "left" }, 1000); 
+            i -= 1;
+            //$("#" + name + i).show();
+            console.info("left_" + i);
+            
+        }
+    });
+
+    $("#arrowRight").click(function () {
+        if (i + 1 < 7) {
+            i += 1;
+            $("#" + name + i).hide("slide", { direction: "left" }, 1000); 
+            //$("#" + name + i).hide();
+            console.info("right_" + i);
+        }
+    });
+});
+
+
+
